2ac48fd52d9058529376adeee96e73bb149dfc5a
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-02-02 09:39:25 +0100

    create hook

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import useMoneda from '../hooks/useMoneda';
+
 const Boton = styled.input`
     margin-top: 20px;
     font-weight: bold;
@@ -20,8 +22,15 @@ const Boton = styled.input`
 `;
 
 const Formulario = () => {
+
+    //Utilizar useMoneda
+    const [ moneda, SelectMonedas, actualizarState ] = useMoneda();
+
     return (
         <form>
+
+            <SelectMonedas />
+            
             <Boton
                 type="submit"
                 value="Calcular"
